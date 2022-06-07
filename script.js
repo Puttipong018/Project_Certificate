8fa25e25665def16752873d1d3a1b9a3fe572b51
Puttipong Aranyapuek <puttipong.a@psru.ac.th>
2022-06-08 02:48:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function vatPrice(){
+    let userPrice = document.getElementById("price_food").value
+    let vatResult = (userPrice * 7) / 100
+
+    document.getElementById("vat_price").innerHTML ="Vat จากค่าอาหาร = " + vatResult.toFixed(2) + " บาท"
+
+    let allPrice = parseInt(userPrice)  + parseInt(vatResult) 
+
+    document.getElementById("all_price").innerHTML = "ราคาที่ต้องจ่ายทั้งหมด = " + allPrice + " บาท"
+}
+
+
+
